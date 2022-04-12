6a42052de29e9b1438b1b5d2a387104768fde790
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-04-12 06:23:01 +0530

    Time: 0 ms (100.00%), Space: 7 MB (20.27%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,66 +1,71 @@
 class Solution {
 public:
+    
     int getLiveNeighbours(vector<vector<int>>& curr, int i, int j, int m, int n){
         
         int liveNeighbours = 0;
-        if(i>0){
+        if(i>0) {
             //check the upper neighbour
-            if(curr[i-1][j] == 1) liveNeighbours++;
+            if(curr[i-1][j] == 1 || curr[i-1][j] == 3) liveNeighbours++;
         }
-        if(i<m-1){
+        if(i<m-1) {
             //check the lower neighbour
-            if(curr[i+1][j] == 1) liveNeighbours++;
+            if(curr[i+1][j] == 1 || curr[i+1][j] == 3) liveNeighbours++;
         }
-        if(j>0){
+        if(j>0) {
             //check the left neighbour
-            if(curr[i][j-1] == 1) liveNeighbours++;
+            if(curr[i][j-1] == 1 || curr[i][j-1] == 3) liveNeighbours++;
         }
-        if(j<n-1){
+        if(j<n-1) {
             //check the right neighbour
-            if(curr[i][j+1] == 1) liveNeighbours++;
+            if(curr[i][j+1] == 1 || curr[i][j+1] == 3) liveNeighbours++;
         }
 
         if(i>0 && j>0) {
-            if(curr[i-1][j-1] == 1) liveNeighbours++;
+            if(curr[i-1][j-1] == 1 || curr[i-1][j-1] == 3) liveNeighbours++;
         }
-        if(i>0 && j<n-1){
-            if(curr[i-1][j+1] == 1) liveNeighbours++;
+        if(i>0 && j<n-1) {
+            if(curr[i-1][j+1] == 1 || curr[i-1][j+1] == 3) liveNeighbours++;
         }
-        if(i<m-1 && j>0){
-            if(curr[i+1][j-1] == 1) liveNeighbours++;
+        if(i<m-1 && j>0) {
+            if(curr[i+1][j-1] == 1 || curr[i+1][j-1] == 3) liveNeighbours++;
         }
-        if(i<m-1 && j<n-1){
-            if(curr[i+1][j+1] == 1) liveNeighbours++;
+        if(i<m-1 && j<n-1) {
+            if(curr[i+1][j+1] == 1 || curr[i+1][j+1] == 3) liveNeighbours++;
         }
         return liveNeighbours;
     }
     
     void gameOfLife(vector<vector<int>>& board) {
         
-        vector<vector<int>> curr = board;
         int m = board.size();
         int n = board[0].size();
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                int neighboursCount = getLiveNeighbours(curr, i, j, m, n);
-                if(board[i][j] == 0){
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                int ln = getLiveNeighbours(board, i, j, m, n);
+                if(board[i][j] == 0) {
                     //currently dead
                     //can become alive if has exactly three live neighbours
-                    if(neighboursCount == 3){
-                        board[i][j] = 1;
+                    if(ln == 3) {
+                        board[i][j] = 2; //newly alive
                     }
                 }
-                else if(board[i][j] == 1){
+                else if(board[i][j] == 1) {
                     //<2 -> die
                     //==2 || ==3 live
                     //>3 ->die
-                    if(neighboursCount < 2 || neighboursCount > 3) {
-                        board[i][j] = 0;
+                    if(ln < 2 || ln > 3) {
+                        board[i][j] = 3; //newly dead
                     }
                 }
             }
         }
-        
+        //final modifications 2->1, 3->0
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(board[i][j] == 2) board[i][j] = 1;
+                else if(board[i][j] == 3) board[i][j] = 0;
+            }
+        }
     }
 };
