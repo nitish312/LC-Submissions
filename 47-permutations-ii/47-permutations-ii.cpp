bdbeb54ec1ac42a278732790208849599131bcc2
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-04-14 11:07:06 +0530

    Time: 4 ms (92.70%), Space: 9.2 MB (62.92%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    void calculatePermutations(vector<int>nums, int index, vector<vector<int>> &ans){
+        
+        // base condition - think of a smallest valid input
+        if(index == nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+        
+        // choice diagram - minimize the input
+        for(int i=index; i<nums.size(); i++){
+            
+            if(nums[i] == nums[index] && i != index) continue;
+            
+            swap(nums[i], nums[index]);
+            calculatePermutations(nums, index+1, ans);
+            
+            // swap(nums[i], nums[index]);
+            // calculatePermutations(nums, ans, index+1); // recursively calling func
+            // swap(nums[i], nums[index]); // backtracking step
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums){
+        
+        sort(nums.begin(), nums.end());        
+        vector<vector<int>> ans;
+        int index = 0;
+        calculatePermutations(nums, index, ans);
+        
+        return ans;
+    }
+};
