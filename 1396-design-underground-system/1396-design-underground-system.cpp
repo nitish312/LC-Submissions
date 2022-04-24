4390990924b7479101ad28cf9dcc2fb62ddd44ab
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-04-24 06:47:43 +0530

    Time: 160 ms (80.36%), Space: 59.4 MB (31.15%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,28 +1,30 @@
 class UndergroundSystem {
 public:
     
-    unordered_map<int, pair<string, int>> checkInMap; // Uid - {StationName, Time}
-    unordered_map<string, pair<int, int>> checkoutMap; // Route - {TotalTime, Count}
+    unordered_map<string, pair<int, int>> avg;
+    unordered_map<int, pair<string, int>> entry;
     
     UndergroundSystem() {
         
     }
     
     void checkIn(int id, string stationName, int t) {
-        checkInMap[id] = {stationName, t};
+        entry[id] = {stationName, t}; 
     }
     
     void checkOut(int id, string stationName, int t) {
-        auto& checkIn = checkInMap[id];
-        string route = checkIn.first + "_" + stationName;
-        checkoutMap[route].first += t - checkIn.second;
-        checkoutMap[route].second += 1;
+        string station1 = entry[id].first;
+        
+        avg[station1 + "-" + stationName].first += t - entry[id].second;
+        avg[station1 + "-" + stationName].second += 1;
     }
     
     double getAverageTime(string startStation, string endStation) {
-        string route = startStation + "_" + endStation;
-        auto& checkout = checkoutMap[route];
-        return (double) checkout.first / checkout.second;
+        string travel = startStation + "-" + endStation;
+        
+        double avgTime = avg[travel].first / (double)avg[travel].second;
+        
+        return avgTime;
     }
 };
 
