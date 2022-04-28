ac348f19ced8b2fc020aa3fcce4868a521c291a0
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-04-28 07:28:28 +0530

    Time: 7 ms (63.57%), Space: 6.8 MB (70.06%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -9,7 +9,7 @@ public:
         dp[0][0] = true;
         
         for(int i=0; i<=m; i++){
-            for(int j=1; j<=n; j++){
+            for(int j=1; j<=n; j++){ // didnt understand if else
                 
                 if(p[j-1] == '*'){
                     dp[i][j] = dp[i][j - 2] || 
