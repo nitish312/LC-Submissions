d9fb9efbb921b0007ab5614c02b3d8563a67816f
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-04-30 06:51:22 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (74.56%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,67 +1,64 @@
 class Solution {
 public:
     
-    void dfs(string start,string end,map<string,double>& mp,map<string,vector<string>>& graph,double& val,map<string,int>& visited,bool& found){
-        visited[start]=1;
-    
-        if(found==true)
-            return ;
-        for(auto child:graph[start]){
-            if(visited[child]!=1){
-                // cout<<start<<" "<<child<<"\n";
-                val*=mp[start+"->"+child];
-                if(end==child){
-                    // cout<<end<<" -- "<<child<<"\n";
-                    found=true;
-                    return ;
-                }
-                dfs(child,end,mp,graph,val,visited,found);
-                if(found==true){
-                    return ;
-                }
-                else{
-                    val/=mp[start+"->"+child];
-                }
-            }
-        }
-       
+    double solve(unordered_map<string, vector<pair<string, double>>> &adj_list, unordered_set<string> &s, string a, string b){
         
-    }
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        vector<double> ans;
-        map<string,double> mp;
-        map<string,vector<string>> graph;
-        for(int i=0;i<equations.size();i++){
-            string u=equations[i][0];
-            string v=equations[i][1];
-            mp[u+"->"+v]=values[i];
-            mp[v+"->"+u]=1/values[i];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+	//If we not able to reach then return -1;
+        if(adj_list.find(a)==adj_list.end()||adj_list.find(b)==adj_list.end())
+        {
+            return -1.0;
         }
-        for(int i=0;i<queries.size();i++){
-            string start=queries[i][0];
-            string end=queries[i][1];
-            if(graph.find(start)==graph.end()||graph.find(end)==graph.end()){
-                ans.push_back(-1);
-            }
-            else{
-                // ans.push_back(1);
-                double val=1;
-                map<string,int> visited;
-                bool found=false;
-                if(start==end){
-                    found=true;
+		//If we reach destination return 1.0 as base case
+        if(a==b)
+        {
+            return 1.0;
+        }
+		//insert in set that these nodes are visited
+        s.insert(a);
+        
+        for(auto it:adj_list[a])
+        {
+		//start traversing into the addj_list that they are connected
+            if(s.count(it.first)==0)
+            {
+			
+			//move to next postion
+                double res= solve(adj_list,s,it.first,b);
+                if(res!=-1)
+                {
+				//Multiply every situation to find valid answer
+                    return res*it.second;
                 }
-                else
-                dfs(start,end,mp,graph,val,visited,found);
-                if(found==true)
-                ans.push_back(val);
-                else
-                    ans.push_back(-1);
-                
             }
+            
+        }
+        return -1.0;
+    }
+    
+    
+    
+    
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        //Creating unodred_map to store value
+        unordered_map<string,vector<pair<string,double>>>adj_list;
+        int n=equations.size();
+        for(int i=0;i<n;i++)
+        {
+		//Putting nodes into the given style
+            adj_list[equations[i][0]].push_back({equations[i][1],values[i]});
+            adj_list[equations[i][1]].push_back({equations[i][0],1.0/values[i]});
+        }
+        
+        vector<double>ans;
+        for(int i=0;i<queries.size();i++)
+        {
+		//Unordered_set for visited nodes
+            unordered_set<string>vis;
+			//Solving each querry and store it in the ans
+           double res=solve(adj_list,vis,queries[i][0],queries[i][1]);
+            ans.push_back(res);
         }
         return ans;
+        
     }
 };
