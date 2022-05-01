5c06be3d943810edd302fc4cd7c574925565db60
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-01 13:19:05 +0530

    Time: 10 ms (35.09%), Space: 5.9 MB (28.68%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        int rows = 1;
+        while(n > 0) {     // iterate till we have coins left
+            rows++;        // move to next row
+            n = n - rows;     // fill current row by subtracting from available coins
+        }
+        return rows - 1; 
+    }
+};
