ac79b29fc9ecbadf90f6aa070be882407f07f02b
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-01 13:21:53 +0530

    Time: 4 ms (68.09%), Space: 6 MB (28.68%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -2,11 +2,18 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         
-        int rows = 1;
-        while(n > 0) {     // iterate till we have coins left
-            rows++;        // move to next row
-            n = n - rows;     // fill current row by subtracting from available coins
+        int start = 1, end = n, ans;
+		long mid, coinsNeeded;
+        while(start <= end) {
+            mid = start + ((end - start) >> 1);  // finding mid of range [l, r]
+            coinsNeeded = (mid * (mid + 1)) >> 1;  // coins needed for 'mid' number of row
+            if(coinsNeeded <= n){
+                start = mid + 1;
+                ans = mid;            // if available coins are sufficient
+            }    
+            else end = mid - 1;  // coins insufficient, eliminate the half greater than mid
         }
-        return rows - 1; 
+        
+        return ans;
     }
 };
