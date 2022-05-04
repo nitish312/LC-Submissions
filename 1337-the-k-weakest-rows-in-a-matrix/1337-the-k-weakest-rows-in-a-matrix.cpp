06a66a3f66961fa4ba69e99a326edb0bce864f24
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-04 05:57:21 +0530

    Time: 18 ms (45.33%), Space: 10.5 MB (77.03%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-     vector<int> Weakest(vector<vector<int> >& mat, int k)
-     {
+     vector<int> Weakest(vector<vector<int>>& mat, int k){
+         
         // no of rows;
         int n = mat.size();
         
         // set is to store pairs of count and index
         // count of 1 in ith row, {count,index};
-        set<pair<int,int> > s;
+        set<pair<int,int>> s;
         
         // set will automatically sort the pair {count, index} in ascending order.
         
-        for(int i = 0; i<n; ++i)
-        {
+        for(int i=0; i<n; ++i){
+            
             // stl function for counting frequency of 1 in current row
             int cnt = count(mat[i].begin(),mat[i].end(),1);
             // inserting cnt and index pair to set
@@ -21,10 +21,10 @@ public:
         
         vector<int> ans;
         // first k weakest row is our answer 
-        for(auto i : s)
-        {
-            if(k == 0)
-                break;
+        for(auto i: s){
+            
+            if(k == 0) break;
+            
             ans.push_back(i.second);
             --k;
         }
