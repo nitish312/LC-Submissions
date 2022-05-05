a040f0701f75a352514585c8dc32609626cf048c
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-05 06:01:10 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (58.19%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -4,16 +4,17 @@ public:
         
     }
     
-    queue<int> q1, q2;
+    queue<int> q1;
     
     void push(int x) {
         
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
+        int sz = q1.size();
+        q1.push(x);
+        while(sz--){
+            
+            q1.push(q1.front());
             q1.pop();
         }
-        swap(q1, q2);
     }
     
     int pop() {
