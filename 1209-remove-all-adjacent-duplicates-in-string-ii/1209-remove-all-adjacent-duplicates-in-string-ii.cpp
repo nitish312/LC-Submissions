ec927bca4dd49287fd8cb7a8f6380f1ee1f79021
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-06 08:48:02 +0530

    Time: 473 ms (5.03%), Space: 13.7 MB (17.19%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     string removeDuplicates(string s, int k) {
         
-        vector<pair<int, char>> stack = {{0, '#'}};
-        for (char c: s) {
-            if (stack.back().second != c) {
-                stack.push_back({1, c});
-            } else if (++stack.back().first == k)
-                stack.pop_back();
-        }
-        string res;
-        for (auto & p : stack) {
-            res.append(p.first, p.second);
-        }
-        
-        return res;
+        stack<pair<char, int> >stk; // It will store a character and its consecutive count
+	stk.push({'#', 0}); // just to reduce extra condition checks.
+	for(int i = 0; i < size(s); i++){
+		// if current character is equal to previous character, store it with incremented consecutive count
+		stk.push({s[i], stk.top().first == s[i] ? stk.top().second + 1  : 1});
+		// if consecutive count equals k, then delete that group of k characters.
+		if(stk.top().second == k) {
+			while(stk.top().first == s[i]) stk.pop();
+			s.erase(i - k + 1, k);                
+			i -= k;
+		}                
+	}
+	return s;
     }
 };
