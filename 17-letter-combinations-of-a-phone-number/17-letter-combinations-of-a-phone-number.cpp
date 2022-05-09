6239ee5b4169255a64ece276ed9b4864232d6492
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-09 05:38:19 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (84.83%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,33 @@
 class Solution {
-public:
 
-    vector<string> answer;
-    map<string, string> mmap =
-    {
-        {"2", "abc"},
-        {"3", "def"},
-        {"4", "ghi"},
-        {"5", "jkl"},
-        {"6", "mno"},
-        {"7", "pqrs"},
-        {"8", "tuv"},
-        {"9", "wxyz"}
-    };
-
-    void getCombination(string comb, string digits)
-    {
-        if(digits.size() == 0) answer.push_back(comb);
-        else
-        {
-            for(int i = 0; i < mmap[ digits.substr(0, 1) ].size(); i++ )
-            {
-                string temp = comb + mmap[digits.substr(0,1)][i];
-                string d = digits.substr(1);
-                getCombination(temp, d);
-            }
+private:
+    void solve(string digit, string output, int index, vector<string>& ans, string mapping[]){
+        //base case
+        if(index >= digit.length()){
+            ans.push_back(output);
+            return ;
+        }
+        
+        int number = digit[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i < value.length(); i++){
+            output.push_back(value[i]);
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
         }
     }
-
-    vector<string> letterCombinations(string digits) 
-    {
-        if(digits.empty()) return answer;
-
-        getCombination("", digits);
-        return answer;
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length() == 0)
+            return ans;
+        
+        string output = "";
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+        
     }
 };
