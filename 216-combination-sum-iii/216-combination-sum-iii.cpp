351797f9ae8216085ffcd54fb8b8f4c34a53da2b
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-10 19:03:15 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (37.26%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     
-    void combination(vector<vector<int>>& result, vector<int> sol, int k, int n){
-
-        if(sol.size() == k && n == 0){ 
+    void help(vector<vector<int>>& res, vector<int>& comb, int k, int n, int sum, int num){
         
-            result.push_back(sol); 
-            return; 
+        if(sum==n && comb.size()==k){
+            res.push_back(comb);
+            return;
         }
         
-        if(sol.size() < k){
-            
-          for(int i = sol.empty() ? 1 : sol.back() + 1; i <= 9; ++i){
-              
-            if(n - i < 0) break;
-              
-            sol.push_back(i);
-            combination(result, sol, k, n - i);
-            sol.pop_back();
-          }
+        if(num >= 10 || sum<0){
+            return;
         }
-  }
+        
+        comb.push_back(num);
+        help(res, comb, k, n, sum+num, num+1);
+        comb.pop_back();
+        help(res, comb, k, n, sum, num+1);
+    }
     
     vector<vector<int>> combinationSum3(int k, int n){
         
-        vector<vector<int>> ans;
-        
-        vector<int> sol;
-        
-        combination(ans, sol, k, n);
-        
-        return ans;
+        vector<vector<int>> res;
+        vector<int> comb;
+        int sum = 0;
+        help(res, comb, k, n, sum, 1);
+        return res;
     }
 };
