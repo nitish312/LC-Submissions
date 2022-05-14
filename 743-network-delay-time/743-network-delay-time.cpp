048e6ae4f4722e0c14e019dc328e0a955c04147e
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-14 13:11:03 +0530

    Time: 109 ms (95.34%), Space: 39.9 MB (82.77%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+    
+    
+    #define pii pair<int , int>
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        vector<int> dist(n + 1, INT_MAX);
-        dist[k] = 0;
-        for (int i = 0; i < n; i++) {
-            for (vector<int> e : times) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
-                    dist[v] = dist[u] + w;
-                }
-            }
+        priority_queue<pii , vector<pii> , greater<pii>>pq;
+       vector<pair<int , int>>adj[n+1];
+        for(int i=0 ; i<times.size() ; i++){
+            adj[times[i][0]].push_back({times[i][1] , times[i][2]});
         }
-
-        int maxwait = 0;
-        for (int i = 1; i <= n; i++)
-            maxwait = max(maxwait, dist[i]);
-        return maxwait == INT_MAX ? -1 : maxwait;
+        
+        
+        pq.push({0 , k});
+        
+        vector<int>dist(n+1 , INT_MAX);
+        dist[k]=0;
+        while(!pq.empty()){
+            auto dis = pq.top().first;
+            auto node = pq.top().second;
+            pq.pop();
+            
+            vector<pair<int , int>>::iterator it;
+           for(it = adj[node].begin() ; it!=adj[node].end() ; ++it){
+               if(dist[it->first]>dis + it->second){
+                   dist[it->first]=dis + it->second;
+                   pq.push({dist[it->first] , it->first});
+               }
+           } 
+            
+        }
+            int d = INT_MIN;
+            for(int i=1 ; i<dist.size() ; i++){
+                d = max(dist[i] , d);
+                if(dist[i]==INT_MAX)
+                    return -1;
+            }
+        return d;
     }
 };
