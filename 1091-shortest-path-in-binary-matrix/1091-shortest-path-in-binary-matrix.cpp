19689ee0a4d9012afc777f56d811cf261f936714
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-16 10:57:17 +0530

    Time: 105 ms (48.73%), Space: 19.3 MB (83.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,45 +1,34 @@
-struct Point{
-    int x;
-    int y;
-    int count;
-};
-
 class Solution {
 public:
     
-    int dx[8] = {1, 1, 1, -1, -1, -1, 0, 0};
-    int dy[8] = {1, 0, -1, 1, -1, 0, 1, -1};
-    
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid){
         
-        if(grid[0][0] == 1) return -1;
-        
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        queue<Point> q;
-        q.push({0, 0, 1});
-        grid[0][0] = -1;
-        
-        while(!q.empty()){
-            
-            Point p = q.front();
-            q.pop();
-            
-            if(p.x == row-1 && p.y == col-1) return p.count;
-            
-            for(int i=0; i<8; i++){
-                
-                int x = p.x + dx[i];
-                int y = p.y + dy[i];
-                
-                if(x >= 0 && y >= 0 && x < row && y < col && grid[x][y] == 0){
-                    q.push({x, y, p.count+1});
-                    grid[x][y] = -1;
+        int dir[8][2] = {{-1,0}, {0,1}, {1,0}, {0,-1}, {-1,-1}, {-1,1}, {1,1},                                  {1,-1}};
+
+        queue<pair<int,int>> q;
+
+        if(grid[0][0] != 0) return -1;
+
+        q.emplace(0,0);
+        int m = grid.size(), n = grid[0].size();
+        grid[0][0] = 1;
+
+        while(q.size()){
+
+            auto [r,c]=q.front();q.pop();
+            if(r==m-1&&c==n-1)return grid[r][c];
+
+            for(auto& d:dir){
+
+                int rr=r+d[0],cc=c+d[1];
+                if(rr>=0&&rr<m&&cc>=0&&cc<n&&grid[rr][cc]==0){
+
+                    grid[rr][cc]=grid[r][c]+1;
+                    q.emplace(rr,cc);
                 }
             }
         }
-        
+
         return -1;
     }
 };
