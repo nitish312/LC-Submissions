577f9cebb7ca7feb28d12135bcb41c084f09d51c
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-16 11:00:17 +0530

    Time: 75 ms (73.42%), Space: 19.3 MB (70.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
     
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid){
-        
-        int dir[8][2] = {{-1,0}, {0,1}, {1,0}, {0,-1}, {-1,-1}, {-1,1}, {1,1},                                  {1,-1}};
+    int dirx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+	int diry[8] = {1, -1, 0, 0, -1, 1, -1, 1};
 
-        queue<pair<int,int>> q;
+	int shortestPathBinaryMatrix(vector<vector<int>>& a) {
+		int n = a.size();
+		if (a[0][0] != 0 || a[n - 1][n - 1] != 0)
+		{
+			return -1;
+		}
+		queue<pair<int, int>>q;
+		q.push({0, 0});
+		int step = 0;
+		int size = 0;
+		int x , y, newx, newy;
+		while (!q.empty())
+		{
+			step++;
+			size = q.size();
+			while (size--)
+			{
+				x = q.front().first;
+				y = q.front().second;
+				q.pop();
+				if (x == n - 1 && y == n - 1)
+				{
+					return step;
+				}
+				for (int i = 0; i < 8; i++)
+				{
+					newx = x + dirx[i];
+					newy = y + diry[i];
+					if (newx >= 0 && newx < n && newy >= 0 && newy < n && a[newx][newy] == 0)
+					{
+						a[newx][newy] = 1;
+						q.push({newx, newy});
+					}
+				}
+			}
+		}
 
-        if(grid[0][0] != 0) return -1;
-
-        q.emplace(0,0);
-        int m = grid.size(), n = grid[0].size();
-        grid[0][0] = 1;
-
-        while(q.size()){
-
-            auto [r,c]=q.front();q.pop();
-            if(r==m-1&&c==n-1)return grid[r][c];
-
-            for(auto& d:dir){
-
-                int rr=r+d[0],cc=c+d[1];
-                if(rr>=0&&rr<m&&cc>=0&&cc<n&&grid[rr][cc]==0){
-
-                    grid[rr][cc]=grid[r][c]+1;
-                    q.emplace(rr,cc);
-                }
-            }
-        }
-
-        return -1;
+		return -1;
     }
 };
