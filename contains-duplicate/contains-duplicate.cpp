d6efddaa60fbe5d4e2f3ab99108bc68279b4e430
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-28 15:59:52 +0530

    Time: 125 ms, Memory: 53.6 MB - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         
-        int arrSize = nums.size();
-        
-        unordered_set<int> st;
-        for(auto a: nums) st.insert(a);
-        
-        int uniqSize = st.size();
-        
-        if(uniqSize == arrSize) return false;
-        else return true;
+        unordered_map<int, int> m;
+
+        for(auto a: nums) m[a]++;
+
+        for(auto a: m) if(a.second != 1) return true;
+
+        return false;
     }
 };
