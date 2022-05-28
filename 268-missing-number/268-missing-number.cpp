e346189751bafc7fb9adbfdccbfb0d7c09d3e730
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-28 10:10:05 +0530

    Time: 101 ms (5.13%), Space: 25.8 MB (5.21%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        set<int> st;
+        
+        for(int i=0; i<n; i++){
+            
+            st.insert(nums[i]);
+        }
+        
+        for(int i=0; i<=n; i++){
+            
+            if(st.find(i) == st.end()) return i;
+        }
+        
+        return -1;
+    }
+};
