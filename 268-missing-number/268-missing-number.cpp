8dc5acf2faf054482ce957f2822a20a491d4a380
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-28 10:16:15 +0530

    Time: 30 ms (40.13%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,17 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        int n = nums.size();
-        
-        int arraySum = 0;
-        
-        for(int a: nums){
-            
-            arraySum += a;
-        } 
-            
-        int realSum = n * (n + 1) / 2;
-        
-        return realSum - arraySum;
+        int sum=0,n=nums.size(),sum_n=0;
+        for(int i=0;i<n; i++) sum+=nums[i];
+        for(int i=1;i<=n;i++) sum_n+=i;
+        return sum_n-sum;
     }
 };
