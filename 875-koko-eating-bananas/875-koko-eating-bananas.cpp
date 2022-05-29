7d9e24748f0bbac9a8e7ed45b089de8911f23053
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-29 10:44:55 +0530

    Time: 77 ms (29.75%), Space: 18.9 MB (25.61%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -3,34 +3,36 @@ public:
     
     int calcTime(vector<int> &piles, int mid){
         
-         int sum = 0;
+        int sum = 0;
          
-        for(int i=0;i<piles.size();i++)
-        {
-            sum+=(piles[i]/mid)+((piles[i]%mid)!=0);
+        for(int i=0; i<piles.size(); i++){
+            
+            sum += (piles[i] / mid) + ( (piles[i] % mid) != 0 );
         }
+        
         return sum;
     }
     
     int minEatingSpeed(vector<int>& piles, int h) {
         
-        int low = 1;
-        int high = *max_element(piles.begin(),piles.end());
+        int minSpeed = 0;
         
-        int ans = 0;
-        while(low<=high)
-        {
-            int mid = (low+high)/2;
-            int time = calcTime(piles, mid);
-            if(time<=h)
-            {  
-                ans = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
+        int start = 1, end = *max_element(piles.begin(),piles.end()), mid;
+        
+        while(start <= end){
             
+            mid = start + (end - start) / 2;
+            
+            int time = calcTime(piles, mid);
+            
+            if(time <= h){
+                
+                minSpeed = mid;
+                end = mid - 1;
+            }
+            else start = mid + 1;
         }
         
-        return ans;
+        return minSpeed;
     }
 };
