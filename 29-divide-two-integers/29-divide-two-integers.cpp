9f7e1eb309c0db52305049f9720a0828a567e7f5
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-30 08:25:19 +0530

    Time: 4 ms (49.70%), Space: 5.9 MB (76.09%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -2,42 +2,11 @@ class Solution {
 public:
     int divide(int dividend, int divisor){
         
-        int ans = 0;
-
-        if(divisor == INT_MIN){
-            
-            if(dividend == divisor) return 1;
-            else return 0;
-        }
-            
-        if(dividend == INT_MIN){
-            
-            if(divisor == 1) return INT_MIN;
-            else if (divisor == -1) return INT_MAX;
-            else dividend += abs(divisor), ans++; 
-        }
-            
-        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        if(dividend == 0) return 0;
+        if(divisor == 1) return dividend;
+        if(divisor == -1) return dividend == INT_MIN ? INT_MAX : 0 - dividend;
+        return dividend / divisor;
         
-        dividend = abs(dividend), divisor = abs(divisor);
         
-        while(dividend >= divisor){ 
-            
-            int t = 1; 
-            int curr_dvs = divisor;
-            int curr_dvd = dividend >> 1;
-            
-            while(curr_dvd >= divisor){
-                
-                curr_dvs = curr_dvs << 1;
-                t = t << 1;
-                curr_dvd = curr_dvd>>1;
-            }
-
-            dividend -= curr_dvs;
-            ans += t;
-        }
-        
-        return sign > 0 ? ans : -ans;
     }
 };
