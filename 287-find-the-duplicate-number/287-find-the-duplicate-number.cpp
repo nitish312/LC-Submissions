a85fa504517039b00eda2c3fb5dec4a09ee7db3d
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-05-30 10:45:41 +0530

    Time: 128 ms (78.66%), Space: 61.3 MB (27.52%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -3,13 +3,22 @@ public:
     
     int findDuplicate(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
+        int start = 1, end = nums.size() - 1, cnt;
         
-        for(int i=0; i<nums.size(); i++){
+        while(start <= end){
             
-            if(nums[i] == nums[i+1]) return nums[i];
+            int mid = start + (end - start) / 2;
+            
+            cnt = 0;
+            
+            for(int n : nums){
+                
+                if(n <= mid) cnt++;
+            }
+            
+            if(cnt <= mid) start = mid + 1;
+            else end = mid - 1;
         }
-        
-        return -1;
+        return start;    
     }
 };
