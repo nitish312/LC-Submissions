4a77340d2c54c28312f5b44584511532009b9029
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-05 10:34:29 +0530

    Time: 8 ms (52.21%), Space: 6.9 MB (26.82%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    int soluCounter = 0;
+    
+    vector<vector<string>> ans;
+    
+    bool isSafe(vector<string> &board, int r, int c){
+        
+        for(int i=0; i<r; i++){
+            
+            if(board[i][c] == 'Q') return false;
+        }
+        
+        int row = r, col = c;
+        while(row >= 0 && col >= 0){
+            
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+        
+        row = r, col = c;
+        while(row >= 0 && col < board.size()){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col++;
+        }
+        
+        return true;
+    }
+    
+    void nQueen(vector<string> &board, int r){
+        
+        if(r == board.size()){
+            ans.push_back(board);
+            soluCounter++;
+            return;
+        }
+        
+        for(int i=0; i<board.size(); i++){
+            
+            if(isSafe(board, r, i)){
+                
+                board[r][i] = 'Q';
+                nQueen(board, r+1);
+                board[r][i] = '.';
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n){
+        
+        if(n <= 0) return {{}};
+        
+        vector<string> board(n, string(n, '.'));
+        
+        nQueen(board, 0);
+        
+        return ans;
+    }
+    
+    int totalNQueens(int n) {
+        
+        solveNQueens(n);
+        return soluCounter;
+    }
+};
