4228fe91dd7f2a22cd4fab30ea4bbbd4dacbd343
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-06 10:46:24 +0530

    Time: 74 ms (34.72%), Space: 14.5 MB (88.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,37 +8,37 @@
  */
 class Solution {
 public:
+    
+    int getLength(ListNode* temp){
+        int len = 0;
+        while(temp){
+            len++;
+            temp = temp->next;
+        } 
+        return len;
+    }
+    
+    ListNode* differenceMove(ListNode* temp, int small, int big){
+        int diff = big - small;
+        while(diff--){
+            temp = temp->next;
+        }
+        return temp;
+    }
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
         ListNode* dummy1 = headA;
         ListNode* dummy2 = headB;
         
-        int len1 = 0, len2 = 0;
-        while(dummy1){
-            
-            len1++;
-            dummy1 = dummy1->next;
-        } 
-        while(dummy2){
-            
-            len2++;
-            dummy2 = dummy2->next;
-        } 
+        int len1 = getLength(dummy1);
+        int len2 = getLength(dummy2);
         
         dummy1 = headA;
         dummy2 = headB;
         
-        int diff;
-        if(len1 < len2){
-            
-            diff = len2 - len1;
-            while(diff--) dummy2 = dummy2->next;
-        }
-        else{
-            
-            diff = len1 - len2;
-            while(diff--) dummy1 = dummy1->next;
-        }
+        if(len1 < len2) dummy2 = differenceMove(dummy2, len1, len2);
+        else dummy1 = differenceMove(dummy1, len2, len1);
         
         while(dummy1 && dummy2){
             
