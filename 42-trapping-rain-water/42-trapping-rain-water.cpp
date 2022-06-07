4a22765f1fecfed159dbcab228e9f38271111783
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-07 19:15:56 +0530

    Time: 23 ms (39.23%), Space: 19.7 MB (32.57%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -4,31 +4,33 @@ public:
         
         int n = height.size();
         
-        int prefix[n], suffix[n];
+        int left = 0, right = n - 1;
+        int ans = 0;
         
-        int first = 0;
-        prefix[first] = height[first];
-        for(int i=1; i<n; i++){
+        int maxLeft = 0, maxRight = 0;
+        
+        while(left <= right){
             
-            int curr = i, prev = i - 1;
-            prefix[curr] = max(prefix[prev], height[curr]);
+            if(height[left] <= height[right]){
+                
+                if(height[left] >= maxLeft)
+                    maxLeft = height[left];
+                else
+                    ans += maxLeft - height[left];
+                
+                left++;                
+            }
+            else{
+                
+                if(height[right] >= maxRight)
+                    maxRight = height[right];
+                else 
+                    ans += maxRight - height[right];
+                
+                right--;
+            }
         }
         
-        int last = n - 1;
-        suffix[last] = height[last];
-        for(int i=n-2; i>=0; i--){
-            
-            int curr = i, prev = i + 1;
-            suffix[curr] = max(suffix[prev], height[curr]);
-        }
-        
-        int trappedWaterSum = 0;
-        
-        for(int i=0; i<n; i++){
-            
-            trappedWaterSum += min(prefix[i], suffix[i]) - height[i];
-        }
-        
-        return trappedWaterSum;
+        return ans;
     }
 };
