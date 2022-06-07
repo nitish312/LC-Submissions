1f0c835722361531f5ea4c0eeac6d1dd51f869c2
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-07 07:41:15 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (95.06%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -16,8 +16,8 @@ public:
         if(!p && !q) return true;
         if(!p || !q) return false;
         
-        return (p->val == q->val && isSameTree(p->left, q->left) 
-                                 && isSameTree(p->right, q->right));
-        
+        return (p->val == q->val && 
+                isSameTree(p->left, q->left) &&
+                isSameTree(p->right, q->right));
     }
 };
