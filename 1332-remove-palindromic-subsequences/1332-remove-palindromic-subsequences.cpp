4e0370adb79c69e4c9b29f5b4fe40ed57bef46a6
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-08 10:10:58 +0530

    Time: 2 ms (43.62%), Space: 6.2 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     
-    bool isPalindrome(string s, int n){
+//     bool isPalindrome(string s, int n){
         
-        int start = 0, end = n - 1;
-        while(start < end){
+//         int start = 0, end = n - 1;
+//         while(start < end){
             
-            if(s[start] != s[end]) return false;
+//             if(s[start] != s[end]) return false;
             
-            start++, end--;
-        }
-        return true;
-    }
+//             start++, end--;
+//         }
+//         return true;
+//     }
     
     int removePalindromeSub(string s) {
         
-        int n = s.size();
+//         int n = s.size();
         
-        if(n == 0) return 0;
-        if(isPalindrome(s, n)) return 1;
-        
-//         unordered_set<char> st;
-        
-//         for(int i=0; s[i]!='\0'; i++) st.insert(s[i]);
+//         if(n == 0) return 0;
+//         if(isPalindrome(s, n)) return 1;
+        string temp = s;
+        reverse(temp.begin(), temp.end());
+        if(temp == s) return 1;
         
         return 2;
     }
