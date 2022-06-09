07562b9f19460bef402f9cdc08cdad9a0aaad565
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-09 11:05:28 +0530

    Time: 15 ms (75.98%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -23,11 +23,11 @@ public:
         
 //         return ans;
         
-        vector<int> ans;
+        // vector<int> ans;
         
         int n = numbers.size();
         
-        int start = 0, end = n - 1;
+        int start = 0, end = n-1;
         while(start < end){
             
             int firstEle = numbers[start];
@@ -35,14 +35,16 @@ public:
             int sum = firstEle + lastEle;
             if(sum == target){
                 
-                ans.push_back(start+1);
-                ans.push_back(end+1);
-                break;
+                // ans.push_back(start);
+                // ans.push_back(end);
+                // break;
+                return {start+1, end+1};
             }
             else if(sum < target) start++;
             else end--;
         }
         
-        return ans;
+        // return ans;
+        return {};
     }
 };
