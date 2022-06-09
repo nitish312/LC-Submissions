b7e4cebac103e05ea9a991db1335e808cf19b98a
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-09 10:59:21 +0530

    Time: 23 ms (30.54%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,22 +3,44 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         
+//         vector<int> ans;
+        
+//         for(int i=0; i<numbers.size()-1; i++){
+            
+//             int start = i+1, end = numbers.size() - 1, gap = target - numbers[i];
+//             while(start <= end){
+                
+//                 int mid = start + (end - start) / 2;
+//                 if(numbers[mid] == gap){
+//                     ans.push_back(i+1);
+//                     ans.push_back(mid+1);
+//                     break;
+//                 }
+//                 else if(numbers[mid] < gap) start = mid + 1;
+//                 else end = mid - 1;
+//             }
+//         }
+        
+//         return ans;
+        
         vector<int> ans;
         
-        for(int i=0; i<numbers.size()-1; i++){
+        int n = numbers.size();
+        
+        int start = 0, end = n - 1;
+        while(start < end){
             
-            int start = i+1, end = numbers.size() - 1, gap = target - numbers[i];
-            while(start <= end){
+            int firstEle = numbers[start];
+            int lastEle = numbers[end];
+            int sum = firstEle + lastEle;
+            if(sum == target){
                 
-                int mid = start + (end - start) / 2;
-                if(numbers[mid] == gap){
-                    ans.push_back(i+1);
-                    ans.push_back(mid+1);
-                    break;
-                }
-                else if(numbers[mid] < gap) start = mid + 1;
-                else end = mid - 1;
+                ans.push_back(start+1);
+                ans.push_back(end+1);
+                break;
             }
+            else if(sum < target) start++;
+            else end--;
         }
         
         return ans;
