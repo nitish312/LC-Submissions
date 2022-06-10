d00355929644bb7d8b2a17eb8f09eeb233589f84
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-10 19:26:43 +0530

    Time: 22 ms (44.89%), Space: 5.9 MB (74.60%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+     
+        if(x < 0) return false;
+        
+        int num = x;
+        long rev = 0;
+        while(num != 0){
+            
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        
+        return (rev == x) ? true : false;
+    }
+};
