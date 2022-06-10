6d03375c25914024c3e2b8437408ffa7d218597f
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-10 10:38:18 +0530

    Time: 91 ms (14.83%), Space: 13.4 MB (18.07%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -10,17 +10,17 @@ public:
         int start = 0, end = 0;
         while(end < n){
             
-            if(st.find(s[end]) == st.end()){
-                
-                st.insert(s[end]);
+            if(st.find(s[end]) == st.end()){ //check if curr char is present in set
+                // if not // if curr char is unique 
+                st.insert(s[end]); // inserting it into set
                 int currStrsz = end - start + 1;
-                maxi = max(maxi, currStrsz);
-                end++;
+                maxi = max(maxi, currStrsz); //update max if current size is maximum
+                end++; // check for next character in string
             }
             else{
-                
-                st.erase(s[start]);
-                start++;
+                // otherwise
+                st.erase(s[start]); // remove that char from start end, so we can have curr char in our substring for next iteration
+                start++; // update start to its next char
             }
         }
         
