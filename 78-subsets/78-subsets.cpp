588dfb7580f01a5ba1d4e69c3a0ede569a94fb81
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-13 12:57:29 +0530

    Time: 4 ms (51.42%), Space: 7.1 MB (69.65%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     
-    void generate(vector<int>& nums, vector<int>& sbst, int i, vector<vector<int>>& allSubsets){
-        
-        if(i == nums.size()){
-            
-            allSubsets.push_back(sbst);
-            return;
-        }
-        
-        // ith element not in subset
-        generate(nums, sbst, i+1, allSubsets);
-        
-        // ith element in subset
-        sbst.push_back(nums[i]);
-        generate(nums, sbst, i+1, allSubsets);
-        sbst.pop_back();
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
         
         vector<vector<int>> allSubsets;
-        vector<int> sbst;
-        generate(nums, sbst, 0, allSubsets);
         
+        int n = nums.size();
+        
+        allSubsets.push_back({});
+        
+        for(int i=0; i<n; i++){
+            
+            int mainVecSize = allSubsets.size();
+            
+            for(int j=0; j<mainVecSize; j++){
+                
+                vector<int> v = allSubsets[j];
+                v.push_back(nums[i]);
+                allSubsets.push_back(v);
+            }
+        }
         return allSubsets;
     }
 };
