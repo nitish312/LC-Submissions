8c53a4e597d4f9258377948938256e6a146b6734
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-13 13:18:44 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (24.30%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -4,13 +4,19 @@ public:
         
         int n = nums.size();
         
-        int start = 0, end = n - 1;
-        int mid = 0;
-        while(mid <= end){
+        int zeroes = 0, ones = 0, twos = 0;
+        
+        for(int i=0; i<n; i++){
             
-            if(nums[mid] == 0) swap(nums[mid++], nums[start++]);
-            else if(nums[mid] == 1) mid++;
-            else if(nums[mid] == 2) swap(nums[mid], nums[end--]);
+            if(nums[i] == 0) zeroes++;
+            else if(nums[i] == 1) ones++;
+            else if(nums[i] == 2) twos++;
         }
+        
+        int i = 0;
+            
+        while(zeroes--) nums[i++] = 0;
+        while(ones--) nums[i++] = 1;
+        while(twos--) nums[i++] = 2;
     }
 };
