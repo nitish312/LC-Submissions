7c9640f17c5123bb57ef8f20dfddcd6d63c9956e
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-15 11:00:01 +0530

    Time: 42 ms (52.25%), Space: 25.1 MB (53.86%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,14 +3,7 @@ public:
     
     void reverseIt(vector<int>& nums, int start, int end){
         
-        while(start < end){
-            
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
+        while(start < end) swap(nums[start++], nums[end--]);
     }
     
     void rotate(vector<int>& nums, int k){
