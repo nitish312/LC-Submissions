1d165fa31d4652c4dfae7719000eb4e2fedb3043
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-18 10:32:41 +0530

    Time: 3 ms (87.24%), Space: 11.1 MB (29.22%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -3,29 +3,25 @@ public:
     
     int search(vector<int>& nums, int target) {
         
-        int start = 0, end = nums.size() - 1;
+        int n = nums.size();
         
+        int start = 0, end = n - 1, mid;
         while(start <= end){
             
-            int mid = start + (end - start) / 2;
+            mid = start + (end - start) / 2;
             
-            if(target == nums[mid]) return mid;
+            if(nums[mid] == target) return mid;
             
-            //if rotation exists, mid ele is in left part of the array
             if(nums[mid] > nums[end]){
                 
-                if (target < nums[mid] && target >= nums[start]) end = mid - 1;
-                else start = mid + 1; 
+                if(nums[mid] > target && target >= nums[start]) end = mid - 1;
+                else start = mid + 1;
             }
-            
-            //if rotation exists, mid ele is in right part of the array
             else if(nums[mid] < nums[start]){
                 
-                if (target > nums[mid] && target <= nums[end]) start = mid + 1;
-                else end = mid - 1; 
+                if(nums[mid] < target && target <= nums[end]) start = mid + 1;
+                else end = mid - 1;
             }
-            
-            // if there is no rotation; dp normal binary search
             else{
                 
                 if(nums[mid] < target) start = mid + 1;
