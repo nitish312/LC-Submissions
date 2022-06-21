29660e0f3134ae10bac72aa1921d0c906cf7f386
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-21 15:42:10 +0530

    Time: 7 ms (6.81%), Space: 6.2 MB (11.86%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    // DP - TopDownn (Tabulation)
-    
     int numTrees(int n) {
-         
-        vector<int> dp(20 , 0);
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2; 
         
-        for(int i=3; i<=n; i++){
+        vector<int> dp(n+1);
+        
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
             
             for(int j=1; j<=i; j++){
                 
-                int left = j - 1;
-                int right = i - (left + 1);
-                dp[i] += dp[left] * dp[right];
+                dp[i] += dp[j-1] * dp[i-j];
             }
         }
-        
+            
         return dp[n];
     }
 };
