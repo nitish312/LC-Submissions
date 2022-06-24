73959baf3efac74ece35c2dc3e0517e40b68be6e
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-24 14:22:13 +0530

    Time: 36 ms (28.11%), Space: 18 MB (18.89%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -4,12 +4,19 @@ public:
      
         int n = nums.size();
         
-        unordered_set<int> allElems;
-        for(auto ele: nums) allElems.insert(ele);
+//         unordered_set<int> allElems;
+//         for(auto ele: nums) allElems.insert(ele);
+        
+//         for(int i=0; i<=n; i++){
+            
+//             if(allElems.find(i) == allElems.end()) return i;
+//         }
+        
+        sort(nums.begin(), nums.end());
         
         for(int i=0; i<=n; i++){
             
-            if(allElems.find(i) == allElems.end()) return i;
+            if(i == n || i != nums[i]) return i;
         }
         
         return -1;
