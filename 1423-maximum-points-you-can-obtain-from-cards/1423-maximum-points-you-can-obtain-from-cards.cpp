964fdbf19121fe0383bbd8c895eb7568380a1fc5
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-06-26 18:16:04 +0530

    Time: 98 ms (36.44%), Space: 42.4 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -4,22 +4,18 @@ public:
         
         int n = cardPoints.size();
     
-        int res = 0;
-		
-		//First k elements in our window
-        for(int i=0; i<k; i++) res += cardPoints[i];
+        int sum =0;
+        for(int i=0; i<k; i++) sum += cardPoints[i];
         
-        int curr = res;
-        for(int i=k-1; i>=0; i--){
+        int maxsum = sum;
+        
+        for(int i=0; i<k; i++){
             
-			//We remove the last visited ele and add the non-visited ele from the last
-            curr -= cardPoints[i];
-            curr += cardPoints[n - k + i];
-			
-            //We check the maximum value any possible combination can give
-			res = max(res, curr);
+            sum += cardPoints[n - i - 1] - cardPoints[k - i - 1];
+            
+            if(sum > maxsum) maxsum = sum;
         }
         
-        return res;
+        return maxsum;
     }
 };
